7e21cebab60d347e5f15df3a9db0985225f00136
FRANCISCO NAKU ACOSTA ZARATE <fconakuaz@gmail.com>
2023-05-24 13:27:33 -0600

    Se agrega auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { HeaderApiKeyStrategy } from './auth-header-api-key.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [PassportModule, ConfigModule],
+  providers: [HeaderApiKeyStrategy],
 })
 export class AuthModule {}
